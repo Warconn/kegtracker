ad3fb44c4975f289840a1900e5aec9453564b3a0
warconn <tyler.conn@hotmail.com>
2019-11-18 00:15:34 -0500

    Displaying the Kegs and buttons

--- kegtracker-angular-ui/src/app/app.component.ts
+++ kegtracker-angular-ui/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {KegtrackingService} from './services/kegtracking.service';
+import Keg from './models/keg.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kegtracker-angular-ui';
+  
+  constructor(
+    //Private todoservice will be injected into the component by Angular Dependency Injector
+    private kegService: KegtrackingService
+  ) { }
+
+  //Declaring the new todo Object and initilizing it
+  public newKeg: Keg = new Keg()
+
+  //An Empty list for the visible todo list
+  kegList: Keg[];
+
+
+  ngOnInit(): void {
+
+    //At component initialization the 
+    this.kegService.getKegs()
+      .subscribe(kegs => {
+        //assign the todolist property to the proper http response
+        this.kegList = kegs
+        console.log(kegs)
+      })
+  }
+
+  pourBeer(keg: Keg) {
+    
+    if(keg.currentvolume > 1){  //current volume is greater than 1
+      console.log("Current Volume: " + keg.currentvolume);
+      //subtract a pint
+      keg.currentvolume--;
+      console.log("New Volume: " + keg.currentvolume);
+
+      this.kegService.editKeg(keg)
+        .subscribe(ret => {
+          console.log(ret);
+        })
+    }
+    console.log(keg.beername +" poured!")
+  }
 }
+
+
--- kegtracker-angular-ui/src/app/app.module.ts
+++ kegtracker-angular-ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { KegtrackingService } from './services/kegtracking.service';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
   ],
   providers: [
     KegtrackingService
