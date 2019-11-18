76d3ca6a2b0ac56e02c24fc885daf6011706869b
warconn <tyler.conn@hotmail.com>
2019-11-17 23:13:52 -0500

    Added angular mats

--- kegtracker-ui/src/app/app.module.ts
+++ kegtracker-ui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [
     KegService
